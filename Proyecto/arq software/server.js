eb84487c39121a571cd9a64cb6843a24fee0a104
Jesus Mantilla <jesusmantilla11.jm@gmail.com>
2025-05-31 12:01:23 -0500

    Configurar puerto dinámico para Render

--- Proyecto/arq software/server.js
+++ Proyecto/arq software/server.js
@@ -84,6 +84,8 @@ app.delete('/eliminar', (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(3000, () => {
-  console.log('🚀 Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
